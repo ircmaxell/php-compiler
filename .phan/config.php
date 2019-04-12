7c8699b1329fa448a216f7eed3b250fa949002d6
Dave MacFarlane <driusan@gmail.com>
2019-04-12 12:49:18 -0400

    Fix reference to undefined variable (#25)
    The variable "$hash" doesn't exist in 3 places in
    HashTable. Presumably these are intended to be "$index".

--- .phan/config.php
+++ .phan/config.php
@@ -13,7 +13,6 @@ return [
 		'PhanTypeInvalidBitwiseBinaryOperator',
 		'PhanTypeObjectUnsetDeclaredProperty',
 		'PhanUnreferencedUseNormal',
-		'PhanUndeclaredVariable',
 		'PhanUndeclaredClassMethod',
 		'PhanTypeMismatchProperty',
 		'PhanTypeExpectedObjectPropAccessButGotNull',
